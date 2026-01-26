7d3de714935fe6a1f89350d834a01bc740a3d747
danielnicolasmuner <danielnicolasmuner@gmail.com>
2026-01-25 20:12:42 -0500

    refactor: remove button component and adjust logo positioning in HowItWorks

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -64,26 +64,8 @@ const HowItWorks = ({
               {steps[activeStep]?.description || ""}
             </p>
             
-            <div className="flex justify-center lg:justify-start mb-6 md:mb-8">
-              <button 
-                onClick={buttonAction}
-                className="button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 px-7 text-n-8"
-              >
-                <span className="relative z-10">{buttonText}</span>
-                <svg className="absolute top-0 left-0" width="21" height="44" viewBox="0 0 21 44">
-                  <path fill="white" stroke="white" strokeWidth="2" d="M21,43.00005 L8.11111,43.00005 C4.18375,43.00005 1,39.58105 1,35.36365 L1,8.63637 C1,4.41892 4.18375,1 8.11111,1 L21,1"></path>
-                </svg>
-                <svg className="absolute top-0 left-[1.3125rem] w-[calc(100%-2.625rem)]" height="44" viewBox="0 0 100 44" preserveAspectRatio="none" fill="white">
-                  <polygon fill="white" fillRule="nonzero" points="100 0 100 44 0 44 0 0"></polygon>
-                </svg>
-                <svg className="absolute top-0 right-0" width="21" height="44" viewBox="0 0 21 44">
-                  <path fill="white" stroke="white" strokeWidth="2" d="M0,43.00005 L5.028,43.00005 L12.24,43.00005 C16.526,43.00005 20,39.58105 20,35.36365 L20,16.85855 C20,14.59295 18.978,12.44425 17.209,10.99335 L7.18,2.77735 C5.792,1.61115 4.147,1 2.466,1 L0,1"></path>
-                </svg>
-              </button>
-            </div>
-
-            {/* Logo replacing the help button */}
-            <div className="flex items-center justify-center lg:justify-start">
+            {/* Logo */}
+            <div className="flex items-center justify-center lg:justify-start mt-6 md:mt-8">
               <div className="grayscale opacity-60 hover:opacity-80 transition-opacity duration-300">
                 <Logo
                   logoSize={{ width: 48, height: 48 }}
